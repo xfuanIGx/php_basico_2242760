66536197434de9c1f3da5ce3d5c3763c85178502
unknown <jmgarcia0849@gmail.com>
2021-04-29 11:37:16 -0500

    arreglos multidimensionales

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,52 @@ Route::get("Arreglos", function () {
     print_r($estudiantes);
     echo "</pre>";
 });
+
+
+Route::get( 'paises' , function(){
+ $paises = [
+
+    "COLOMBIA" => [
+
+        "Capital" => "Bogota",
+        "Moneda" => "Pesos",
+        "Poblacion" => 50.34
+    ],
+
+    "PERU" => [
+
+        "Capital" => "Lima",
+        "Moneda" => "sol",
+        "Poblacion" => 32.51
+
+    ],
+
+    "Paraguay" => [
+
+        "Capital" => "Asuncion",
+        "Moneda" => "Guarani",
+        "Poblacion" => 7.2
+
+    ]
+
+    ];
+
+foreach($paises as $nombrepais => $pais){
+
+    echo "<h1>$nombrepais</h1>";
+    echo"<pre>";
+    print_r($pais["Moneda"]);
+    echo "<br>";
+    print_r($pais["Poblacion"]);
+    echo"</pre>";
+    echo "<hr />";
+
+}
+
+});
+
+
+
+
+
+
